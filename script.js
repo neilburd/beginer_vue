5517e5701b2aee303b7ba38dd1acf6bfec90f67d
“neilburd” <neilburd@gmail.com>
2017-01-29 15:54:47 -0500

    created this project

--- script.js
+++ script.js
@@ -2,5 +2,10 @@ var app = new Vue({
   el: '#app',
   data: {
     message: 'You loaded this page on'
+  },
+  methods: {
+    reverseMessage: function(){
+      this.message = this.message.split('').reverse().join('')
+    }
   }
 })
